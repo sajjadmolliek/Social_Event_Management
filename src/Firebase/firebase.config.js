492866aaab4e1d2d1bd3820a70d4ef852027602b
sajjadmolliek <sajjadmolliek2018@gmail.com>
2023-10-09 01:28:29 +0600

    Hiding Firebase.config.js File content

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -5,24 +5,16 @@ import { getAuth } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: import.meta.env.VITE_APIKEY,
-//   authDomain: import.meta.env.VITE_AUTHDOMAIN,
-//   projectId: import.meta.env.VITE_PROJECTID,
-//   storageBucket: import.meta.env.VITE_STORAGEBUCKET,
-//   messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
-//   appId: import.meta.env.VITE_APPID,
-// };
-
-
 const firebaseConfig = {
-  apiKey: "AIzaSyAhHz3M_tI1eOMwBSngAmOK1vJfr-XpFOs",
-  authDomain: "social-event-management-8ae1a.firebaseapp.com",
-  projectId: "social-event-management-8ae1a",
-  storageBucket: "social-event-management-8ae1a.appspot.com",
-  messagingSenderId: "490225076419",
-  appId: "1:490225076419:web:490179f301939ba59a7a76"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
+
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
